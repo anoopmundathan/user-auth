fce9a78ccbf14cf18af8d63847236484663ab289
anoopmundathan <anoop.mundathan@gmail.com>
2017-02-08 07:52:31 +0000

    Hash the password

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -25,6 +26,17 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
+UserSchema.pre('save', function(next) {
+	var user = this;
+	bcrypt.hash(user.password, 10, function(err, hash) {
+		if(err) return next(err);
+		user.password = hash;
+		next();
+	});
+});
+
 var User = mongoose.model('User', UserSchema);
+
+
 
 module.exports = User;
